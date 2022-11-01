9f9a9227e64615983f4822770e8c8387fe269086
Rafhael Cardoso <100536547+rafhaelcardoso@users.noreply.github.com>
2022-11-01 00:37:08 -0300

    Create Produto Model

---
+++ ProdutosApi.Backend/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProdutosApi.Backend.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+    public string Imagem { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public DateTime DataEdicao { get; set; }
+}
